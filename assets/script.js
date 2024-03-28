fd28f059938158980b1271704cb0c1fd96e83a05
Matheus M. Prado <131068785+Matheus-m-prado@users.noreply.github.com>
2024-03-27 21:08:38 -0300

    start

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// MENU ANIMATION
+const toggleBtn = document.querySelector(".toggle-btn");
+const toggleBtnIcon = document.querySelector(".toggle-btn i");
+const dropdownmenu = document.querySelector(".dropdown-menu");
+
+toggleBtn.onclick = function(){
+	dropdownmenu.classList.toggle('open');
+	const isOpen = dropdownmenu.classList.contains('open');
+
+	toggle.classList = isOpen
+	? 'fa-solid fa-xmark'
+	: 'fa-solid fa-bars';
+}
